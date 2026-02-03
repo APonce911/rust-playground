cba35c6d35da34c4166bacf47ed08d38ecb9a537
airton <ponceairton@gmail.com>
2026-02-03 14:01:48 +0100

    using match expression, pattern matching & variable shadowing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,11 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
   println!("Guess the number!");
 
   let secret_number = rand::thread_rng().gen_range(1..=10);
-  println!("secret number: {secret_number}");
 
   loop {
     println!("Please input your guess.");
@@ -15,11 +15,14 @@ fn main() {
       .expect("Failed to read line");
     println!("You guessed: {guess}");
 
-    let guess_number: u32 = guess.trim().parse().unwrap();
-    if guess_number == secret_number {
-      break;
-    } else {
-      println!("Try Again!");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+          break;
+        }
     }
   }
 
