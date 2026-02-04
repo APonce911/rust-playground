5e0a75daad4c317d3e35870d8f8d02e0640bea83
airton <ponceairton@gmail.com>
2026-02-04 16:29:01 +0100

    include ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    let literal = "hello";
+    println!("{literal}");
+    print_type_of(&literal); // &str
+
+    let mut s = String::from(literal);
+    print_type_of(&s); // alloc::string::String
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+    // literal.push_str(", world!"); // do not work
+
+    println!("{s}"); // this will print `hello, world!`
+
+    let s1 = String::from("Rust"); // store data in the Heap
+    let s2 = s1; // s1 becomes an ivalid reference. Rust "moved" s1 to s2
+
+    // println!("{s1}, is cool!"); // do not work, because s1 has become an invalid reference to prevent memory corruption
+    println!("{s2}, is cool!");
+  }
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
