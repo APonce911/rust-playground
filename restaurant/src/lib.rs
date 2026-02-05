b8f7a32beaa4037bd3f7bbe438a8e13cc34436be
airton <ponceairton@gmail.com>
2026-02-05 16:05:10 +0100

    refactor restaurant modules into separate files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -13,49 +13,9 @@ mod tests {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub struct Breakfast {
-      pub toast: String,
-      seasonal_fruit: String,
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches")
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::deliver_order();
-    }
-
-    fn cook_order() {}
-}
+mod back_of_house;
 
 // use crate::front_of_house::hosting;
 pub use crate::front_of_house::hosting as host;
