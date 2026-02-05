4ec12f354aca3bc365293d069063d79521729e9d
airton <ponceairton@gmail.com>
2026-02-05 14:34:35 +0100

    access function from sibling module child

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,8 +14,8 @@ mod tests {
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -27,4 +27,12 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
