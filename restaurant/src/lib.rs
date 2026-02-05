1b05d713a01207596cc054c807c9c02d1c136633
airton <ponceairton@gmail.com>
2026-02-05 15:42:29 +0100

    use hosting alias

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -57,20 +57,27 @@ mod back_of_house {
     fn cook_order() {}
 }
 
-use crate::front_of_house::hosting;
+// use crate::front_of_house::hosting;
+use crate::front_of_house::hosting as host;
 
 pub fn eat_at_restaurant() {
     // Absolute path
     // crate::front_of_house::hosting::add_to_waitlist();
 
     // using hosting shortcut
-    hosting::add_to_waitlist();
+    // hosting::add_to_waitlist();
+
+    // using hosting alias
+    host::add_to_waitlist();
 
     // Relative path
     // front_of_house::hosting::add_to_waitlist();
 
     // using hosting shortcut
-    hosting::add_to_waitlist();
+    // hosting::add_to_waitlist();
+
+    // using hosting alias
+    host::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast.
     let mut meal = back_of_house::Breakfast::summer("Rye");
