7808b168c165a1c1d5d67864fe206b0e0afb3296
airton <ponceairton@gmail.com>
2026-02-09 22:18:00 +0100

    implement async request_with_client

--- bitreq_test/src/main.rs
+++ bitreq_test/src/main.rs
@@ -6,5 +6,23 @@ fn main() {
     assert_eq!(200, response.status_code);
     assert_eq!("OK", response.reason_phrase);
 
-    // println!("{}", response.as_str().unwrap())
+    println!("{}", response.status_code);
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .build()
+        .expect("failed to build Tokio runtime");
+
+    runtime.block_on(request_with_client(url));
+}
+
+async fn request_with_client(url: &str) {
+    let client = bitreq::Client::new(1);
+    let response = client.send_async(bitreq::get(url)).await.unwrap();
+
+    assert!(response.as_str().unwrap().contains("</html>"));
+    assert_eq!(200, response.status_code);
+    assert_eq!("OK", response.reason_phrase);
+
+    println!("{}", response.status_code);
 }
