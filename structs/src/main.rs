104cfb42dc28f2b66c397933f0d3c503d9992ab1
airton <ponceairton@gmail.com>
2026-02-11 11:35:35 +0100

    struct builder

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,5 +14,17 @@ fn main() {
     };
 
     user1.email = String::from("anotherusername123@example.com");
-    println!("{}", user1.email)
+    println!("{}", user1.email);
+
+    let mut user2 = build_user("user2email@example.com".to_string(), "user2username".to_string());
+    println!("{}", user2.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
 }
