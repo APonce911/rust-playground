f26b8ced93f02e804315b79158e8d9e09cb8d8e6
airton <ponceairton@gmail.com>
2026-02-11 18:57:22 +0100

    multi-thread mutex with Arc - atomic reference-counted

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,14 +1,32 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    // mutual exclusion 
-    let m = Mutex::new(5);
+    // Mutex (mutual exclusion): guard data with locks
+    let mutex = Mutex::new(0);
 
-    {
-        let mut num = m.lock().unwrap();
-        // type mutex<i32>
-        *num = 6;
+    // RC (reference-counted) value: Share value ownership(see cap 15)
+    // but RC is not thread safe a change to count can be interrupted by other thread
+    // let counter = Rc::new(m); // wont work
+
+    // use Arc(Atomic reference-counted)
+    let counter = Arc::new(mutex);
+
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            // type mutex<i32>
+            *num += 1;
+        });
+        handles.push(handle);
     }
 
-    println!("m = {m:?}");
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
