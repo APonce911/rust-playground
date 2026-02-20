9d7c424520e775454b3c2a44d0fe740a64660707
airton <ponceairton@gmail.com>
2026-02-20 15:57:50 +0100

    bitreq_test: Update with features for native-tls and rustls

--- bitreq_test/src/main.rs
+++ bitreq_test/src/main.rs
@@ -1,6 +1,19 @@
+#[cfg(any(feature = "rustls", feature = "native-tls"))]
 fn main() {
     let url = "https://airtonponce.com";
-    let response = bitreq::get(url).with_timeout(10).send().unwrap();
+    
+    one_shot_request(&url).expect("failed request");
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_io()
+        .build()
+        .expect("failed to build Tokio runtime");
+
+    let _ = runtime.block_on(request_with_client(&url));
+}
+
+fn one_shot_request(url: &str) -> Result<(), bitreq::Error>{
+    let response = bitreq::get(url).with_timeout(10).send()?;
 
     assert!(response.as_str().unwrap().contains("</html>"));
     assert_eq!(200, response.status_code);
@@ -8,15 +21,10 @@ fn main() {
 
     println!("{}", response.status_code);
 
-    let runtime = tokio::runtime::Builder::new_current_thread()
-        .enable_io()
-        .build()
-        .expect("failed to build Tokio runtime");
-
-    runtime.block_on(request_with_client(url));
+    Ok(())
 }
 
-async fn request_with_client(url: &str) {
+async fn request_with_client(url: &str) -> Result<(), bitreq::Error>{
     let client = bitreq::Client::new(1);
     let response = client.send_async(bitreq::get(url)).await.unwrap();
 
@@ -25,4 +33,5 @@ async fn request_with_client(url: &str) {
     assert_eq!("OK", response.reason_phrase);
 
     println!("{}", response.status_code);
+    Ok(())
 }
