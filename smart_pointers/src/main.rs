69a4e4618fe4ec7ae1a5d438d0e8ea571c6a6449
airton <ponceairton@gmail.com>
2026-02-22 11:50:32 +0100

    smart_pointers: Custom smart pointer with deref trait

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,4 +1,5 @@
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
 enum List {
     // Use Box smart pointer on recursive types, 
@@ -6,6 +7,26 @@ enum List {
     // the exact of data, only the pointer size(udata) 
     Cons(i32, Box<List>),
     Nil,
+}
+
+
+// tuple type with single element T
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    // associated type(https://doc.rust-lang.org/book/ch20-03-advanced-types.html#type-synonyms-and-type-aliases)
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // reference inner data - access MyBox tuple at index 0
+        &self.0
+    }
 }
 
 fn main() {
@@ -21,5 +42,11 @@ fn main() {
     assert_eq!(5, x);
     assert_eq!(5, *z);
     // deref operator works the same for boxes and references(&)
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
     assert_eq!(5, *y);
 }
